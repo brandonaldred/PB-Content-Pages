5e4cc2285d91365e679d4d2fccbaa8a2e2588a83
Brandon Aldred <brandonaldred@gmail.com>
2021-04-19 06:26:43 -0700

    creating builder
    Beginning of building out the landing page builder

--- js/builder.js
+++ js/builder.js
@@ -1,34 +1,24 @@
-const container = document.getElementById('lp-builder');
-container.innerHTML = `<div class="lp-builder-div"><h3>Build Landing Style</h3><img src="img/landing.jpg" /></div>
-<div class="lp-builder-div"><h3>Build Blog Style</h3><img src="img/blog.jpg" /></div>`;
-
-
-
-
-container.addEventListener('click', (e) => {
-    let buildType = '';
-    if (e.target.firstElementChild === null) {
-        if(e.target.parentNode.firstElementChild.textContent === 'Build Landing Style') {
-            landingStyle();
-        } else if (e.target.parentNode.firstElementChild.textContent === 'Build Blog Style') {
-            buildType = 'blog';
-        }
-    } else if (e.target.firstElementChild.textContent === 'Build Landing Style') {
-        landingStyle();
-    } else if (e.target.firstElementChild.textContent === 'Build Blog Style') {
-        buildType = 'blog';
+const builder = document.getElementById('lp-builder');
+const builderModal = document.getElementById('lp-builder-modal');
+builder.addEventListener('click', (e) => {
+    console.log(e.target.id);
+    if(e.target.id === 'lp-builder-modal') {
+        builderModal.style.display = 'none';
     }
-
-    console.log(buildType);
+    if(e.target.tagName === 'P') {
+        console.log('P Tag');
+    }
+    if(e.target.tagName === 'H3') {
+        console.log('H3 Tag');
+    }
+    if(e.target.tagName === 'A') {
+        e.preventDefault();
+        console.log('A Tag');
+    }
+    if(e.target.tagName === 'IMG') {
+        console.log('IMG Tag');
+    }
+    if(e.target.className === 'lp-hero-overlay') {
+        console.log(e.target.nextElementSibling.src)
+    }
 });
-
-
-
-function landingStyle() {
-    container.innerHTML = '';
-    const mainDiv = document.createElement('div');
-    const input = document.createElement('input');
-    input.type = 'text';
-    mainDiv.appendChild(input);
-    container.appendChild(mainDiv);
-}
