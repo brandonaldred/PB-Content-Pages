ecca39f68f7df97c6bf54486ce25d53245c1f20e
Brandon Aldred <brandonaldred@gmail.com>
2021-04-28 08:55:16 -0700

    finished out classes now adding form elements

--- js/builder.js
+++ js/builder.js
@@ -1,3 +1,95 @@
+class ProductGrid {
+    constructor(type,h3,aText,aLink,subItems,footLink,footText) {
+        this.type = type;
+        this.h3 = h3;
+        this.aText = aText;
+        this.aLink = aLink;
+        this.subItems = subItems;
+        this.footLink = footLink;
+        this.footText = footText;
+    }
+}
+
+class Product {
+    constructor(type,p,aText,aLink,img) {
+        this.type = type;
+        this.p = p;
+        this.aText = aText;
+        this.aLink = aLink;
+        this.img = img;
+    }
+}
+
+class ThreeCol {
+    constructor(type,h3,aText,aLink,subItems) {
+        this.type = type;
+        this.h3 = h3;
+        this.aText = aText;
+        this.aLink = aLink;
+        this.subItems = subItems;
+    }
+}
+
+class Column {
+    constructor(h3,p,img) {
+        this.h3 = h3;
+        this.p = p;
+        this.img = img;
+    }
+}
+
+class Paragraph {
+    constructor(type,h3,p,img) {
+        this.type = type;
+        this.h3 = h3;
+        this.p = p;
+        this.img = img;
+    }
+}
+
+class FullWidthInfo {
+    constructor(type,h3,p,aText,aLink,img,opt,align) {
+        this.type = type;
+        this.h3 = h3;
+        this.p = p;
+        this.aText = aText;
+        this.aLink = aLink;
+        this.img = img;
+        this.opt = opt;
+        this.align = align;
+    }
+}
+
+class FullWidthHero {
+    constructor(type,h3,p,aText,aLink,img,align,overlay) {
+        this.type = type;
+        this.h3 = h3;
+        this.p = p;
+        this.aText = aText;
+        this.aLink = aLink;
+        this.img = img;
+        this.align = align;
+        this.overlay = overlay;
+    }
+}
+
+class LeadIn {
+    constructor(type,h3,p,img) {
+        this.type = type;
+        this.h3 = h3;
+        this.p = p;
+        this.img = img;
+    }
+}
+
+class Video {
+    constructor(type,videoId) {
+        this.type = type;
+        this.videoId = videoId;
+    }
+}
+
+
 const build = {
     leadIn: (object) => {
         const leadIn = builder('DIV', [{ atr: 'className', val: 'lp-lead-in' }]);
@@ -160,53 +252,8 @@ const build = {
 
 //Defining page array of objects
 let page = [];
-let element = {
-    type: 'productGrid',
-    h3: 'This is a header',
-    aText: 'This is a link',
-    aLink: '/linklocation',
-    subItems: [ {
-        img: 'https://images.amain.com/cdn-cgi/image/width=475/images/large/bikes/specialized/60121-0234.jpg',
-        type: 'product',
-        p: 'This is product 1',
-        aText: '/product1link',
-        aLink: 'Product 1 Link'
-    },{
-        img: 'https://images.amain.com/images/contentpages/performancebike/specialized/img/ROAD-2475-Hero1.jpg',
-        type: 'lifestyle',
-        p: 'This is product 2',
-        aText: '/product2link',
-        aLink: 'Product 2 Link'
-    }, {
-        img: 'https://images.amain.com/cdn-cgi/image/width=475/images/large/bikes/specialized/60121-0234.jpg',
-        type: 'product',
-        p: 'This is product 1',
-        aText: '/product1link',
-        aLink: 'Product 1 Link'
-    }, {
-        img: 'https://images.amain.com/cdn-cgi/image/width=475/images/large/bikes/specialized/60121-0234.jpg',
-        type: 'product',
-        p: 'This is product 1',
-        aText: '/product1link',
-        aLink: 'Product 1 Link'
-    },{
-        img: 'https://images.amain.com/images/contentpages/performancebike/specialized/img/ROAD-2475-Hero1.jpg',
-        type: 'lifestyle',
-        p: 'This is product 2',
-        aText: '/product2link',
-        aLink: 'Product 2 Link'
-    }],
-    footLink: 'footlinklocation',
-    footText: 'this is a footer link',
-    html: ''
-}
-page.push(element);
 
-
-for (let i = 0; i < page.length; i++) {
-    build[page[i].type](page[i]);
-    document.getElementById('lp-container').appendChild(page[i].html);
-}
+page.push();
 
 function builder(item, atr) {
     item = document.createElement(item);
